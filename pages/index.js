574095ba26392f568de1040088abb04c0d6f0f5e
team-cake <eka.cheuk@gmail.com>
2020-12-04 15:33:37 +0100

    Adjusted content in index.js

--- pages/index.js
+++ pages/index.js
@@ -27,8 +27,8 @@ export default function Home({ allPostsData }) {
 					solve!
 				</p>
 				<p>
-					(This is a sample website - you’ll be building a site like this on{' '}
-					<a href='https://nextjs.org/learn'>our Next.js tutorial</a>.)
+					(This is a sample website - you can build a site like this with{' '}
+					<a href='https://nextjs.org/learn'>this Next.js tutorial</a>.)
 				</p>
 			</section>
 			<section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
