13b578f97379e4334bfc3833c6cdb685fc111e18
team-cake <eka.cheuk@gmail.com>
2020-12-05 16:31:06 +0100

    Convert index.js to index.tsx typescript

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,33 +4,36 @@ import utilStyles from '../styles/utils.module.css';
 import { getSortedPostsData } from '../lib/posts';
 import Link from 'next/link';
 import Date from '../components/date';
+import { GetStaticProps } from 'next';
 
-export async function getStaticProps() {
+export const getStaticProps: GetStaticProps = async () => {
 	const allPostsData = getSortedPostsData();
 	return {
 		props: {
 			allPostsData,
 		},
 	};
-}
+};
 
-export default function Home({ allPostsData }) {
+export default function Home({
+	allPostsData,
+}: {
+	allPostsData: {
+		date: string;
+		title: string;
+		id: string;
+	}[];
+}) {
 	return (
 		<Layout home>
 			<Head>
-				<title>Next Tutorial</title>
+				<title>{siteTitle}</title>
 			</Head>
 			<section className={utilStyles.headingMd}>
+				<p>[Your Self Introduction]</p>
 				<p>
-					Hi, my name is Alex, a creative turned into software developer. Go check
-					out my <a href='https://www.github.com/team-cake'>Github</a>. Got some time
-					to spare? I've made a{' '}
-					<a href='https://team-cake.github.io/sudoku/'>Sudoku app</a>, go forth and
-					solve!
-				</p>
-				<p>
-					(This is a sample website - you can build a site like this with{' '}
-					<a href='https://nextjs.org/learn'>this Next.js tutorial</a>.)
+					(This is a sample website - you’ll be building a site like this in{' '}
+					<a href='https://nextjs.org/learn'>our Next.js tutorial</a>.)
 				</p>
 			</section>
 			<section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
